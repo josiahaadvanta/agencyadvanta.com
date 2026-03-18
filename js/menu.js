2e77dcf96c7bb9db0e4f3d6c9226563d93ff879f
Harun Jasarevic <>
2026-03-18 21:53:44 +0100

    d

--- js/menu.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector(".burger");
-const navp = document.querySelector(".nav_points");
-
-burger.addEventListener("click", () => {
-  burger.classList.toggle("active");
-  navp.classList.toggle("active");
-});
